fb889b17f61e70f2d0f350904eb54a366e5635ed
RatiborChirkov <134150913+RatiborChirkov@users.noreply.github.com>
2024-03-20 02:29:08 +0300

    Готово

--- scanner.py
+++ scanner.py
@@ -1,15 +1,36 @@
 import socket
-from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
-N = 2**16 - 1
-
-for port in range(1,100):
-    sock = socket.socket()
+def scan_port(host, port):
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)  # Установка тайм-аута для подключения
+        result = sock.connect_ex((host, port))
+        if result == 0:
+            return port
         sock.close()
+    except:
+        pass
+
+def progress_bar(total, scanned):
+    percent = (scanned / total) * 100
+    print(f"Прогресс: [{int(percent)}%]", end='\r')
+
+def main():
+    host = input("Введите имя хоста/IP-адрес: ")
+    open_ports = []
+    total_ports = 65535  # Всего портов для сканирования
+
+    scanned_ports = 0
+    for port in range(1, total_ports + 1):
+        scanned_ports += 1
+        progress_bar(total_ports, scanned_ports)
+        port_result = scan_port(host, port)
+        if port_result:
+            open_ports.append(port_result)
+    
+    print("\nОткрытые порты:", sorted(open_ports))
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,20 +1,34 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def handle_client(conn, addr):
+    try:
+        print('Подключение клиента', addr)
+        
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print('Прием данных от клиента {}: {}'.format(addr, data.decode()))
+            conn.sendall(data)
+            
+    except socket.error as e:
+        print(f"Ошибка при работе с клиентом {addr}: {e}")
+        
+    finally:
+        conn.close()
+        print('Отключение клиента', addr)
 
-msg = ''
+def create_socket_and_listen():
+    global sock
+    sock = socket.socket()
+    sock.bind(('', 9099))
+    sock.listen(5)
+    print('Начало прослушивания порта')
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    while True:
+        conn, addr = sock.accept()
+        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+        client_thread.start()
 
-print(msg)
-
-conn.close()
+create_socket_and_listen()
