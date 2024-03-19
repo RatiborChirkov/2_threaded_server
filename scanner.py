7a46ef031dc89bf16cf6ab287eea2c860162f69d
RatiborChirkov <134150913+RatiborChirkov@users.noreply.github.com>
2024-03-20 02:31:44 +0300

    Доделал немного

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@ import socket
 
 def scan_port(host, port):
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket()
         sock.settimeout(1)
         result = sock.connect_ex((host, port))
         if result == 0:
